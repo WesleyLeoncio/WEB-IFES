aa3fb8d4d63d42da9a6528eae1299de72efc6618
Wesley <wesley.leoncio.izi@gmail.com>
2020-02-20 09:21:16 -0300

    Versão atualizada com PHP

---
+++ HTML/OlaMundo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name ="author"
+        content = "Wesley" />
+        <title>Ola Mundo</title>
+    </head>
+    <body>
+        <h1>Ola mundo!</h1>
+        <?php
+          echo "Estou estudando no Ifes"
+        ?>
+        <p> Hoje é dia 20/02/2020 </p>
+        <?= "Cachoeiro de Itapemirim" ?>
+    </body>
+</html>
