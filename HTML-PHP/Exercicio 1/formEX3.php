dcac520619efd9c57075704d397e3be42759c842
Wesley Leoncio <wesley.leoncio.izi@gmail.com>
2020-03-12 20:26:49 -0300

    Atualização

--- HTML-PHP/Exercicio 1/formEX3.php
+++ HTML-PHP/Exercicio 1/formEX3.php
@@ -3,7 +3,7 @@
 <head>
   <title>:: Formulário 3 ::</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="CSS/ex2.css">
+  <link rel="stylesheet" href="CSS/ex3.css">
 </head>
 <body>
     <form action="" name="formEX3" id="formEX3" method="post">
@@ -12,21 +12,24 @@
              <td colspan="2"><label class="titulo">PAGAMENTO</label></td>
            </tr> 
            <tr>
-              <td><label for="iNumero">Número: </label></td></td>
+              <td><label class="ngText" for="iNumero">Número: </label></td></td>
               <td><input name="nNumero"type="text" value="" id="iNumero"/></td>
            </tr>
 
            <tr>
-              <td><label for="iBandeira">Bandeira: </label></td>
-              <td>
-                <input name="nBandeira" type="radio" value="VISA" id="iVisa"/><label for="iBandVisa"></label><img width="50" height="50" src="Image/visa.png" alt="Visa"></br>
-                <input name="nBandeira" type="radio" value="Master Card" id="iMaster"/><label for="iBandMasterCard"></label><img width="50" height="50" src="Image/mastercard.png" alt="Master Card"></br>
-                <input name="nBandeira" type="radio" value="American Express" id="iAmerican"/><label for="iBandAmerican"></label><img width="50" height="50" src="Image/american.png" alt="American Express"></br>  
+              <td><label class="ngText" for="iBandeira">Bandeira: </label></td>
+              <td class="Bandeira">
+                <input name="nBandeira" type="radio" value="VISA" id="iVisa"/><label for="iBandVisa"></label>
+                  <img id="imgVisa" width="50" height="50" src="Image/visa.png" alt="Visa">
+                <input name="nBandeira" type="radio" value="Master Card" id="iMaster"/><label for="iBandMasterCard"></label>
+                  <img id="imgMaster" width="50" height="50" src="Image/mastercard.png" alt="Master Card">
+                <input name="nBandeira" type="radio" value="American Express" id="iAmerican"/><label for="iBandAmerican"></label>
+                  <img id="imgAmerican" width="50" height="38WS" src="Image/american.png" alt="American Express">  
               </td>
             </tr>
 
         <tr>
-        <td><label for="iValidade">Validade: </label></td>
+        <td><label class="ngText" for="iValidade">Validade: </label></td>
         <td>
            <select name="nValidade">
                 <option value="ano2015">2015</option>
